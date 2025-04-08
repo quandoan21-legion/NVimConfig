f7d5975cc8683fecf2b662f53414db053653f49a
quandoan21-legion <qdoan21.legion@gmail.com>
2025-04-08 15:05:26 +0700

    change tab space quantity

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,4 @@
-return {
-	-- Mason for LSP/DAP/formatter installer
+return { -- Mason for LSP/DAP/formatter installer
 	{
 		"williamboman/mason.nvim",
 		build = ":MasonUpdate",
@@ -14,6 +13,7 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"pyright",
+					"lua_ls",
 					"ts_ls",
 					"lemminx",
 				},
@@ -37,16 +37,47 @@ return {
 			})
 
 			-- 🟨 JavaScript / TypeScript
-			-- lspconfig.tsserver.setup({
-			--   capabilities = capabilities,
-			-- })
-
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
+				on_attach = function(_, bufnr)
+					-- 👇 Set tab width to 4 spaces
+					vim.bo[bufnr].tabstop = 4
+					vim.bo[bufnr].shiftwidth = 4
+					vim.bo[bufnr].softtabstop = 4
+					vim.bo[bufnr].expandtab = true
+				end,
 			})
 			-- 📄 XML
 			lspconfig.lemminx.setup({
 				capabilities = capabilities,
+			})
+			-- 🌙 Lua
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" }, -- Avoid "undefined global 'vim'"
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						format = {
+							enable = true,
+							defaultConfig = {
+								indent_style = "space",
+								indent_size = "4",
+							},
+						},
+					},
+				},
+				on_attach = function(_, bufnr)
+					vim.bo[bufnr].tabstop = 4
+					vim.bo[bufnr].shiftwidth = 4
+					vim.bo[bufnr].softtabstop = 4
+					vim.bo[bufnr].expandtab = true
+				end,
 			})
 		end,
 	},
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -61,7 +61,6 @@ return {
 					},
 				},
 			})
-
 			-- Enable Telescope extensions if they are installed
 			pcall(require("telescope").load_extension, "fzf")
 			pcall(require("telescope").load_extension, "ui-select")
